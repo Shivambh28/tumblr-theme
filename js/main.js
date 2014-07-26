a2a94d47d6f4ba9485262be7e73d41fc32c3c9b4
Shivam Bhalla <Shivam@Shivams-MacBook-Air.local>
2014-07-26 00:14:52 -0400

    angular init

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-angular
-  .module('app', [
-    'ui.router'
-  ])
-  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'pages/home.html',
-        controller: 'homeCtrl'
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'pages/about.html',
-        controller: 'aboutCtrl'
-      })
-      .state('contact', {
-        url: '/contact',
-        templateUrl: 'pages/contact.html',
-        controller: 'contactCtrl'
-      })
-  }])
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	//set up the routes
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'homeCtrl'
+		})
+		.when('/about', {
+			templateUrl: 'pages/about.html',
+			controller: 'aboutCtrl'
+			
+		})
+		.when('/contact', {
+			templateUrl: 'pages/about.html',
+			controller: 'contactCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+});
+
+app.controller('homeCtrl', function($scope) {
+	$scope.message = 'home page!';
+});
+
+app.controller('aboutCtrl', function($scope) {
+	$scope.message = 'about page!';
+});
+
+app.controller('contactCtrl', function($scope) {
+	$scope.message = 'contact page!';
+});
